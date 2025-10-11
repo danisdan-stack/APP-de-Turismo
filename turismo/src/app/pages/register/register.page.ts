2ce84e9627971eb76ec3fedbbc8c07502ff7d5fb
Juliana Choque <juliana.choque0234@gmail.com>
2025-10-11 18:16:35 -0300

    se creo la pagina del registro
    hice el backend y un pequeño front para simular el registro de un nuevo usuario

--- turismo/src/app/pages/register/register.page.ts
+++ turismo/src/app/pages/register/register.page.ts
@@ -1,16 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Auth } from '../../services/auth'; 
 
-@Component({
-  selector: 'app-register',
-  templateUrl: './register.page.html',
-  styleUrls: ['./register.page.scss'],
-  standalone: false
-})
-export class RegisterPage implements OnInit {
+@Component({ 
+  selector: 'app-registro', // El selector que se va a usar en otras páginas
+  templateUrl: './register.page.html', // Indica  HTML
+  styleUrls: ['./register.page.scss'], // Indica  CSS
+  standalone:false
+ })
+export class RegisterPage  {
+  // 1. Variables para almacenar los datos del formulario
+  nombre = ''; 
+  apellido = ''; 
+  telefono = ''; 
+  email = '';
+  password = '';
+  confirmPassword = '';
 
-  constructor() { }
+  constructor(private authService: Auth) { } // 2. Inyectas el servicio
 
-  ngOnInit() {
+  async onRegister() {
+    try {
+      // 3. Llama a la lógica de tu compañero
+      await this.authService.register(this.email, this.password);
+      // Éxito: Navegar al login o a la página principal
+      console.log('Registro exitoso!');
+      // TODO: Aquí deberías navegar a otra página usando el Router
+    } catch (e: any) {
+      // 4. Muestra el error de Firebase al usuario
+      console.error('Fallo al registrar:', e.message);
+      // TODO: Mostrar una alerta de Ionic con el mensaje de error (e.message)
+    }
   }
-
 }
