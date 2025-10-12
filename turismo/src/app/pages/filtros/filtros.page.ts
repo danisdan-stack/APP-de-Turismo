c96f6ae93fb71285bb76b6f920bae49880b620c8
Gustavo Ledesma <gustavo.lede@gmail.com>
2025-10-11 21:56:32 -0300

    flitros

---
+++ turismo/src/app/pages/filtros/filtros.page.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-filtros-page',
+  templateUrl: './filtros.page.html',
+  standalone: false,
+})
+export class FiltrosPage {
+  
+  constructor(private router: Router) {}
+
+  onFiltrosAplicados(filtros: any) {
+    console.log('Redirigiendo al mapa con:', filtros);
+    this.router.navigate(['/tabs/mapa'], {
+      state: { filtros: filtros }
+    });
+  }
+}
