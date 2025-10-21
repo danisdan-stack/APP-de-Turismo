861ac888a0414665fe00686ad139e972eb8b547e
Gustavo Ledesma <gustavo.lede@gmail.com>
2025-10-20 23:57:44 -0300

    Update environment.prod.ts

--- turismo/src/environments/environment.prod.ts
+++ turismo/src/environments/environment.prod.ts
@@ -1,3 +1,16 @@
 export const environment = {
-  production: true
+  // MUY IMPORTANTE: Cambiar a 'true' para indicar que es el entorno de producción.
+  production: true, 
+
+  // La configuración de Firebase es la misma, ya que es la base de datos a la que quieres acceder.
+  firebaseConfig: {
+    apiKey: "AIzaSyBRzbhxa21snh-QmsJFi4BxvMlfco5C_-c",
+    authDomain: "turismo-2a3b2.firebaseapp.com",
+    databaseURL: "https://turismo-2a3b2-default-rtdb.firebaseio.com",
+    projectId: "turismo-2a3b2",
+    storageBucket: "turismo-2a3b2.firebasestorage.app",
+    messagingSenderId: "656465806473",
+    appId: "1:656465806473:web:3cf4ab419f5de12faef258",
+    measurementId: "G-F5HP68PJZ6"
+  }
 };
